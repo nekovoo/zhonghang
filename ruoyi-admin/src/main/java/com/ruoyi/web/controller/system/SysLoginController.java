3a25b94941a363bf05cb760127627fcd69afc37b
nekovo <1398993569@qq.com>
2024-09-21 11:50:32 +0800

    新增接口-注册

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysLoginController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/system/SysLoginController.java
@@ -2,7 +2,11 @@ package com.ruoyi.web.controller.system;
 
 import java.util.List;
 import java.util.Set;
+
+import com.ruoyi.common.utils.StringUtils;
+import com.ruoyi.system.service.ISysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,6 +37,9 @@ public class SysLoginController
 
     @Autowired
     private SysPermissionService permissionService;
+
+    @Autowired
+    private ISysUserService userService;
 
     /**
      * 登录方法
@@ -82,5 +89,49 @@ public class SysLoginController
         Long userId = SecurityUtils.getUserId();
         List<SysMenu> menus = menuService.selectMenuTreeByUserId(userId);
         return AjaxResult.success(menuService.buildMenus(menus));
+    }
+
+    /**
+     * 公共的用户注册
+     *
+     * @param user 注册信息
+     * @return 结果
+     */
+    @PostMapping("/public/register")
+    public AjaxResult register(@Validated @RequestBody SysUser user) {
+        // 检查用户名是否唯一
+        if (!userService.checkUserNameUnique(user)) {
+            return AjaxResult.error("注册失败，登录账号已存在");
+        }
+        // 检查手机号码是否唯一
+        else if (StringUtils.isNotEmpty(user.getPhonenumber()) && !userService.checkPhoneUnique(user)) {
+            return AjaxResult.error("注册失败，手机号码已存在");
+        }
+        // 检查邮箱是否唯一
+        else if (StringUtils.isNotEmpty(user.getEmail()) && !userService.checkEmailUnique(user)) {
+            return AjaxResult.error("注册失败，邮箱账号已存在");
+        }
+
+        // 设置默认昵称，如果没有提供，设置为用户名
+        if (StringUtils.isEmpty(user.getNickName())) {
+            user.setNickName(user.getUserName());
+        }
+
+        // 设置默认角色为普通用户
+        Long[] defaultRoleIds = {2L};
+        user.setRoleIds(defaultRoleIds);
+
+        // 设置加密密码
+        user.setPassword(SecurityUtils.encryptPassword(user.getPassword()));
+        // 设置创建者信息
+        user.setCreateBy("public"); // 或者是"system"等
+        // 插入用户
+        int result = userService.insertUser(user);
+
+        if (result > 0) {
+            return AjaxResult.success("用户注册成功");
+        } else {
+            return AjaxResult.error("用户注册失败，请稍后再试");
+        }
     }
 }
